51fbab370608b8a8df3395c4c05f4a1ef333c9f0
Nyarlathotepss <rcp.nmillet@gmail.com>
2020-08-19 09:37:23 +0200

    updated api_request.py

--- api_request.py
+++ api_request.py
@@ -7,6 +7,6 @@ class Api:
     def request_post_api_with_auth(self, temperature):
         """send temperature for one user in Django db"""
         url = 'http://164.90.234.183/api/temperatures/'
-        headers = {'Authorization': 'Token 54d6868af13438d6f16571547badf900a76b5bf8'}
+        headers = {'Authorization': 'Token votre_token'}
         data = {'temperature': temperature}
         requests.post(url, data=data, headers=headers)
